5f8e90fa88734d104b63cd612d21e6a7113f47f4
Naman1219 <41509737+Naman1219@users.noreply.github.com>
2021-06-21 00:05:35 +0530

    uploaded new js file

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ for(var i=0;i<numberOfDrumButtons;i++){
   {
       var buttonInnerHTML = this.innerHTML;
       makeSound(buttonInnerHTML);
+      buttonAnimation(buttonInnerHTML);
 
   });
 }
@@ -18,6 +19,7 @@ for(var i=0;i<numberOfDrumButtons;i++){
 document.addEventListener("keydown", function(event)
 {
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 
@@ -26,41 +28,54 @@ function makeSound(key)
 {
   switch(key){
     case "w":
-    var tom1 = new Audio("tom-1.mp3");
+    var tom1 = new Audio("sounds/tom-1.mp3");
     tom1.play();
     break;
 
     case "a":
-    var tom2 = new Audio("tom-2.mp3");
+    var tom2 = new Audio("sounds/tom-2.mp3");
     tom2.play();
     break;
 
     case "s":
-    var tom3 = new Audio("tom-3.mp3");
+    var tom3 = new Audio("sounds/tom-3.mp3");
     tom3.play();
     break;
 
     case "d":
-    var tom4 = new Audio("tom-4.mp3");
+    var tom4 = new Audio("sounds/tom-4.mp3");
     tom4.play();
     break;
 
     case "j":
-    var snare = new Audio("snare.mp3");
+    var snare = new Audio("sounds/snare.mp3");
     snare.play();
     break;
 
     case "k":
-    var crash = new Audio("crash.mp3");
+    var crash = new Audio("sounds/crash.mp3");
     crash.play();
     break;
 
     case "l":
-    var kick = new Audio("kick-bass.mp3");
+    var kick = new Audio("sounds/kick-bass.mp3");
     kick.play();
     break;
 
     default: console.log(buttonInnerHTML);
   }
+
+}
+
+
+
+function buttonAnimation(currentKey)
+{
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function()  {
+    activeButton.classList.remove("pressed");
+  }, 100);
 
 }
